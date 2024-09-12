0d8bfb264ec2871f2c2b0d3f6a67574d2f19dff3
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-09-12 14:22:27 +0530

    login register on client finish

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,15 +1,45 @@
-import "./login.scss";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import './login.scss';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  // const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    try {
+      const res = await axios.post('http://localhost:8000/api/auth/login', {
+        username,
+        password,
+      });
+      console.log(res.data);
+      // navigate('/login');
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
+          <button disabled={isLoading}>Login</button>
+          {error && <span>{error} </span>}
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
