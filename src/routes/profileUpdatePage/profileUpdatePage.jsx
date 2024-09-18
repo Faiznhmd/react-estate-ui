da5e19f7994204498f7437f2913f62b1e1701b61
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-09-18 10:12:20 +0530

    posts created

--- src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -8,7 +8,7 @@ import CloudinaryUploadWidget from '../../components/upload/uploadWidget.jsx';
 function ProfileUpdatePage() {
   const { currentUser, updateUser } = useContext(AuthContext);
   const [error, setError] = useState('');
-  const [avatar, setAvatar] = useState(currentUser.avatar);
+  const [avatar, setAvatar] = useState([]);
 
   const navigate = useNavigate();
 
@@ -22,7 +22,7 @@ function ProfileUpdatePage() {
         username,
         email,
         password,
-        avatar,
+        avatar: avatar[0],
       });
       updateUser(res.data);
       navigate('/profile');
@@ -65,7 +65,11 @@ function ProfileUpdatePage() {
         </form>
       </div>
       <div className="sideContainer">
-        <img src={avatar || '/noimg.jpg'} alt="" className="avatar" />
+        <img
+          src={avatar[0] || currentUser.avatar || '/noimg.jpg'}
+          alt=""
+          className="avatar"
+        />
 
         {/* cloudinary img */}
 
@@ -77,7 +81,7 @@ function ProfileUpdatePage() {
             maxImageFileSize: 200000,
             folder: 'avatars',
           }}
-          setAvatar={setAvatar}
+          setState={setAvatar}
         />
       </div>
     </div>
